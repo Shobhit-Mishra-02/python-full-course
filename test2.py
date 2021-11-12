ee6a59807ca37dbb8c0cdbc8f1bb93a184362a08
shobhit mishra <shobhitmishra2002@gmail.com>
2021-11-12 17:21:42 +0530

    added test2.py file

---
+++ test2.py
@@ -0,0 +1,36 @@
+
+# print() is function and it is used to printing something on the screen.
+
+"""
+this is 
+hello
+sdfdf
+"""
+
+'''
+this is 
+multiline comment
+'''
+
+# print("hello, friend..this is my code")
+
+x = 3
+# print(x)
+
+# rule no. 1
+var = 4
+Var = 5
+Var3 = "raman"
+# print(var)
+# print(Var3)
+
+# rule no. 2
+var_4 = "underscore"
+# _var_4 = "underscore"
+
+print(var_4)
+
+# rule no. 3
+# Do not use any key word
+
+# var 6 = 3
