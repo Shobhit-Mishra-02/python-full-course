58ee10b19c4ef145f596ad1d24efb9f8455e99e0
shobhit mishra <shobhitmishra2002@gmail.com>
2021-11-18 17:45:36 +0530

    added the test3

---
+++ test3.py
@@ -0,0 +1,47 @@
+
+# int (0,2,-4)
+# x = 4
+# x = -4
+# d_type = type(x)
+# print(d_type)
+
+# float
+# x = 3.4
+# print(type(x))
+
+# complex
+# x = 3 + 4j
+# print(type(x))
+
+# numeric [int, float, complex]
+
+# Boolean data type[True, False]
+# x = True
+# x = False
+# print(type(x))
+
+# Sequence [string, list, tuple]
+# lt = [2, 4, "hello"]
+# print(type(lt))
+# print(lt[2])
+
+# st = "hello"  # 4
+# print(type(st))
+# print(st[4])
+
+# tu = (2, 4, 3.6, "raman")
+# print(type(tu))
+# print(tu[0])
+
+# Dictionary
+
+# dic = {
+#     "name": "kabir",
+#     "age": 12,
+#     "school": "abc school"
+# }
+
+# print(type(dic))
+# print(dic["name"])
+# print(dic["age"])
+# print(dic["school"])
