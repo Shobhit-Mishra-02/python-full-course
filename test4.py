db51d2e032d405047983c87b587db4acf3c4c0b9
shobhit mishra <shobhitmishra2002@gmail.com>
2021-11-21 11:31:27 +0530

    Added file test4

---
+++ test4.py
@@ -0,0 +1,36 @@
+# x = 2
+# change = float(x)
+# print(x)
+# print(change)
+
+
+# x = 2.4
+# x = 2.9
+# change = int(x)
+# print(x)
+# print(change)
+
+
+# x = 2.4
+# change = str(x)
+# print(x)
+# print(change)
+# print(type(change))
+
+# lt = [1, 2, 3]
+# lt = (1, 2, 3)
+# lt = "what is your name?"
+# print(list(lt))
+# print(tuple(lt))
+
+dic = {
+    "name": "kabir",
+    "age": 12,
+    "class": "12th",
+    "array": [1, 2, 3]
+}
+
+print(dic.values())
+
+# print(list(dic.values()))
+# print(tuple(dic))
