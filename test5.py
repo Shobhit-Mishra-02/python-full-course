d22d6c69ff0f6440a59b3df95814c5755d3460c8
shobhit mishra <shobhitmishra2002@gmail.com>
2021-11-26 09:48:42 +0530

    added test5 file

---
+++ test5.py
@@ -0,0 +1,53 @@
+# Arithmatic op
+x = 4
+y = 2
+z = x + y
+z = x - y
+z = x * y
+z = x / y
+z = x // y
+z = x ** y  # x^y
+z = x % y
+# print(z)
+
+# comparision op
+# print(x > y)
+# print(x < y)
+# print(x <= y)
+# print(x >= y)
+# print(x != y)
+# print(x == y)
+
+# assigment op
+z = 3
+# z = x
+# print(x)
+# print(z)
+# z += 1 # z = z + 1
+# z -= 1  # z = z - 1
+# z *= 2  # z = z *2
+# z /= 2  # z = z /2
+# z **= 2  # z = z ** 2
+# z %= 2  # z = z % 2
+# z //= 2  # z = z // 2
+# print(z)
+
+# logical op
+# print(True and False)
+# print(True and True)
+# print(False and False)
+# print(True or False)
+# print(True or True)
+# print(False or False)
+var = False
+# print(not var)
+
+# identity op
+# print(x is y)
+# print(x is not y)
+
+# membership op
+# print(3 in [3, 4, 5])
+# print(30 in [3, 4, 5])
+# print(30 not in [3, 4, 5])
+# print(3 not in [3, 4, 5])
