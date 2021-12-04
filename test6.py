b3d36c45be6ec7396916dff39ec27a8cabf7dfcc
shobhit mishra <shobhitmishra2002@gmail.com>
2021-12-04 20:59:26 +0530

    added file test6

---
+++ test6.py
@@ -0,0 +1,37 @@
+age = 60
+
+# if statements
+# if age > 18:
+#     print("you are eligible for voting")
+
+# if else statements
+# if age > 18:
+#     print("you are eligible for voting")
+# else:
+#     print("you are not eligible for voting")
+
+# else if statements
+# if age > 18:
+#     print("you are eligible for voting")
+# elif age == 18:
+#     print("you are 18")
+# elif age != 18:
+#     print("you are not 18")
+# else:
+#     print("you are not eligible for voting")
+
+
+# nested if else statement
+# if age > 18:
+#     if age > 99:
+#         print("we need to award you")
+#     else:
+#         print("you are eligible for voting")
+# else:
+#     print("you are not eligible for voting")
+
+# if (age > 18) and (age < 25):
+#     print("you are in the range")
+
+if (age > 18) or (age < 25):
+    print("you are in the range")
