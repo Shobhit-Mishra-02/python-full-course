1b3665989859fb5127d01bb132b2f859aa2121cd
shobhit mishra <shobhitmishra2002@gmail.com>
2021-12-07 21:22:44 +0530

    added test7 file

---
+++ test7.py
@@ -0,0 +1,17 @@
+# x = 3
+# y = 5
+# s = x + y
+# print("sum:", s)
+
+# x = input()
+# print("the value in x is:", x)
+# print("data type:", type(x))
+
+# x = int(input("Enter first number:"))
+# y = int(input("Enter second number:"))
+# s = x + y
+# print("sum:", s)
+
+# st = input("Enter your name:")
+num = float(input("Enter a floating number:"))
+print(num)
