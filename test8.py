6096b4ba4b7ff0add36ebbd9d38c1242e1e7f034
shobhit mishra <shobhitmishra2002@gmail.com>
2021-12-12 16:16:05 +0530

    added test8 and test9 files

---
+++ test8.py
@@ -0,0 +1,31 @@
+st = 'this is me!'
+
+# print(st)
+# print(st)
+# print(st)
+# print(st)
+
+# for i in range(0, 4):#0 to 3
+#     print(i)
+#     print(st)
+
+# for i in range(4):#0 to 3
+#     print(i)
+#     print(st)
+
+
+# for i in range(2, 4):  # 0 to 3
+#     print(i)
+#     print(st)
+
+# for i in range(1, 10, 2):# 1 to 9
+#     print(i)
+
+# for i in range(5, 0, -1):
+#     print(i)
+# for i in range(0, -6, -1):
+#     print(i)
+
+# for i in range(10):
+# print('2 *', i+1, '=', 2*(i+1))
+# print(f"2 X {i+1} = {2*(i+1)}")
---
+++ test9.py
@@ -0,0 +1,16 @@
+st = 'this is me!'
+
+i = 0
+# while i < 4:
+#     print(st)
+#     print(i)
+#     i += 1
+i = 0
+# while i >= -5:
+#     print(i)
+#     i -= 1
+
+# i = 5
+# while i > 0:
+#     print(i)
+#     i -= 1
