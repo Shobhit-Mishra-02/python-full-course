bafd946244de9a47f5dc9bd8fe6674d5b45d1f5e
shobhit mishra <shobhitmishra2002@gmail.com>
2022-01-02 17:05:55 +0530

    added test file 12

---
+++ test12.py
@@ -0,0 +1,32 @@
+lt = [1, 2, 3, [2, 3], "coding", 9.76]
+# print(type(lt))
+
+# print(lt[0])
+# print(lt[1])
+# print(lt[2])
+# print(lt[3])
+
+# print(len(lt))
+# print(lt[len(lt)-1])
+
+# print(lt[0:4])
+# print(lt[0:5])
+# print(lt[1:5])
+# print(lt[3:5])
+# print(lt[3:])
+
+# print(lt[:5])
+# print(lt[:5:1])
+# print(lt[:5:2])
+# print(lt[:5:3])
+
+# print(lt[-1])
+# print(lt[-2])
+# print(lt[-3])
+
+# print(lt[-5:-1])
+# print(lt[-5:])
+# print(lt[:-3])
+# print(lt[:-2])
+# print(lt[:-2:1])
+# print(lt[:-2:2])
