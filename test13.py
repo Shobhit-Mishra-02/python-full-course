b5ecf5123c932be2891543629e1f6c6f9e5947f4
shobhit mishra <shobhitmishra2002@gmail.com>
2022-01-08 09:33:08 +0530

    added test13 file

---
+++ test13.py
@@ -0,0 +1,27 @@
+lt = [2, 3, 1, 6, 6, 60]
+
+# lt.append(45)
+
+# print(lt)
+# lt.clear()
+
+# lt.sort()
+# lt.sort(reverse=True)
+
+# x = lt.index(6)
+# print(x)
+
+# lt.pop()
+# lt.pop(1)
+
+# lt.insert(0, 123)
+
+# x = lt.count(6)
+# print(x)
+
+# length = len(lt)
+
+# x = max(lt)
+# x = min(lt)
+# x = sum(lt)
+# print(x)
